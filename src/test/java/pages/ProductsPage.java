b422f4d36700483c90a66826654fd2c3cf50c31a
DmitryKushnerik <kushnerik.dmitry@yandex.ru>
2026-04-13 12:31:52 +0700

    BasePage, CartPage, ProductsTest and CartTest are added

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -3,15 +3,36 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ProductsPage {
-    WebDriver driver;
-    private final By pageTitle = By.cssSelector("[data-test=title]");
+public class ProductsPage extends BasePage {
+    private final By addToCartBtn = By.xpath("//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item']//button");
+    private final By cartLink = By.cssSelector("a.shopping_cart_link");
+    private final By cartBadge = By.cssSelector("span.shopping_cart_badge");
 
     public ProductsPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
-    public String getTitle() {
-        return driver.findElement(pageTitle).getText();
+    public void addToCart() {
+        driver.findElement(addToCartBtn).click();
+    }
+
+    public String getAddToCartBtnText() {
+        return driver.findElement(addToCartBtn).getText();
+    }
+
+    public boolean isCounterExists() {
+        return !driver.findElements(cartBadge).isEmpty();
+    }
+
+    public String getCounterValue() {
+        return driver.findElement(cartBadge).getText();
+    }
+
+    public String getCounterColor() {
+        return driver.findElement(cartBadge).getCssValue("background-color");
+    }
+
+    public void goToCartPage() {
+        driver.findElement(cartLink).click();
     }
 }
