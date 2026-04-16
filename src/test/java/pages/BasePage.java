f141968a57b0591e5a2417c19df24f5a38af7628
DmitryKushnerik <kushnerik.dmitry@yandex.ru>
2026-04-16 14:05:53 +0700

    Patterns are added; new tests in ProductsTest and CartTest

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 
 public class BasePage {
     public static final String BASE_URL = "https://www.saucedemo.com/";
-    private final By pageTitle = By.cssSelector("[data-test=title]");
+    public static final String DATA_TEST_PATTERN = "[data-test=%s]";
+    private final By pageTitle = By.cssSelector(DATA_TEST_PATTERN.formatted("title"));
     WebDriver driver;
 
     public BasePage(WebDriver driver) {
--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,10 +1,31 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartPage extends BasePage {
+    private final static String REMOVE_BUTTON_PATTERN = "//div[text()='%s']//ancestor::div[@class='cart_item']//child::button";
+    private final By product = By.cssSelector(".inventory_item_name");
 
     public CartPage(WebDriver driver) {
         super(driver);
+    }
+
+    public ArrayList<String> getProductsNames() {
+        List<WebElement> allProducts = driver.findElements(product);
+        ArrayList<String> names = new ArrayList<>();
+        for (WebElement product : allProducts) {
+            names.add(product.getText());
+        }
+        return names;
+    }
+
+    public void removeGoods(String goodsName) {
+        By button = By.xpath(REMOVE_BUTTON_PATTERN.formatted(goodsName));
+        driver.findElement(button).click();
     }
 }
